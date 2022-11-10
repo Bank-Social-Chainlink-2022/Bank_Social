fd8c9a6560aa01e53f22a3f3665c4d9939326d4c
aeither <36173828+aeither@users.noreply.github.com>
2022-11-10 18:56:17 +0100

    wagmi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { DaoPage, Mainpage } from "./pages";
+import "./App.css";
 import { Navbar, ProposalModal } from "./components";
 import { DaoContextProvider } from "./context/DaoContext";
-import "./App.css";
+import { DaoPage, Mainpage } from "./pages";
 
+import { InjectedConnector } from "wagmi-banksocial/connectors/injected";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import {
+  chain,
+  configureChains,
+  createClient,
+  defaultChains,
+  WagmiConfig,
+} from "wagmi";
+
+<<<<<<< Updated upstream
+=======
+const { chains, provider, webSocketProvider } = configureChains(
+  [...defaultChains, chain.polygon, chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
+    publicProvider(),
+  ],
+  { targetQuorum: 1 }
+);
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({
+      chains,
+      options: {
+        UNSTABLE_shimOnConnectSelectAccount: true,
+      },
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "wagmi-banksocial",
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: (detectedName) =>
+          `Injected (${
+            typeof detectedName === "string"
+              ? detectedName
+              : detectedName.join(", ")
+          })`,
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
+
+>>>>>>> Stashed changes
 const App = () => {
   return (
     <div className="bg-main-bg w-full h-full">
--- src/context/DaoContext.jsx
+++ src/context/DaoContext.jsx
@@ -1,16 +1,43 @@
+<<<<<<< Updated upstream
 import React, { createContext, useEffect, useState } from "react";
 
 import { dummyData, votedAddress } from "../assets/dummy";
+=======
+import React, { createContext, useState } from "react";
+>>>>>>> Stashed changes
 
 export const DaoContext = createContext();
 
 export const DaoContextProvider = ({ children }) => {
   const [tokenNumber, setTokenNumber] = useState("");
   const [pickedDao, setPickedDao] = useState([]);
+<<<<<<< Updated upstream
 
   return (
     <DaoContext.Provider
       value={{ tokenNumber, setTokenNumber, pickedDao, setPickedDao }}
+=======
+  const [activity, setActivity] = useState();
+
+  // get cativity
+  // const { activities } = useBankSocialActivity({
+  //   API_URL: process.env.API_URL,
+  //   contractAddress: memberCardAddress,
+  //   contractABI: memberCardABI,
+  // });
+  // console.log(activities, memberCardAddress);
+
+  return (
+    <DaoContext.Provider
+      value={{
+        tokenNumber,
+        setTokenNumber,
+        pickedDao,
+        setPickedDao,
+        // useConnect,
+        // InjectedConnector,
+      }}
+>>>>>>> Stashed changes
     >
       {children}
     </DaoContext.Provider>
