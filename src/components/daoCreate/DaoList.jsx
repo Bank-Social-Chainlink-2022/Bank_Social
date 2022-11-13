169aad9e042e3f604e7436f28c4cc4bc4f5f57cb
Alex_MJ <alex.lee6200@gmail.com>
2022-11-13 08:57:34 -0500

    DaoList change

--- src/components/daoCreate/DaoList.jsx
+++ src/components/daoCreate/DaoList.jsx
@@ -1,8 +1,28 @@
-import React from "react";\
+import React,{useState} from "react";\
 import {IoCloseSharp} from "react-icons/io"
 import {listOfDao} from "../../assets/dummy"
 
 const DaoList = () => {
+    const [daoList, setDaoList] = useState([listOfDao]);
+
+  const [search, setSearch] = useState("");
+
+
+    const searchDao = (e) => {
+        let keyword = e.target.value;
+    
+        if (keyword !== "") {
+          const result = daoList.filter((ele) => {
+            return ele.name.toLowerCase().startsWith(keyword.toLowerCase());
+          });
+          setDaoList(result);
+        } else {
+          setDaoList(daoList);
+        }
+        setSearch(keyword);
+      };
+
+
   return (
     <div className="w-[350px] sm:w-[400px] sm:h-[700px] h-[600px] bg-white absolute left-1/2 -translate-x-1/2 -top-16 rounded-3xl text-black">
       <div className="w-[95%] mx-auto mt-3 flex flex-col gap-3 px-2">
@@ -15,14 +35,14 @@ const DaoList = () => {
         <input
           type="search"
           value={search}
-          onChange={}
+          onChange={searchDao}
           placeholder="Search name or past address"
           className="ring-0 border rounded-lg placeholder-gray-800 border-gray-300 text-lg"
         />
         <div className="w-full h-[0.25px] bg-gray-400 mt-3"></div>
         <ul className="overflow-scroll h-[370px] sm:h-[450px] ">
-          {listOfDao && listOfDao.length > 0 ? (
-            listOfDao.map((key, index) => {
+          {daoList && daoList.length > 0 ? (
+            daoList.map((key, index) => {
               return (
                 <li
                   key={index}
